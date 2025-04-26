aba3ad628e4c011ce782ad2e88c7dad35cb74e74
Alexander Burnett <alexburnett214321@gmail.com>
2025-04-26 15:14:06 -0400

    clientupdate

--- GameClient.py
+++ GameClient.py
@@ -9,42 +9,36 @@ def client_program():
     pygame.display.set_caption('Client Input Window')
 
     print("Trying to connect to server")
-    host = "192.168.0.227"
+    host = "192.168.1.140"
     port = 5000
 
     client_socket = socket.socket()
     client_socket.connect((host, port))
-
-    keys_held = {
-        pygame.K_a: False,
-        pygame.K_d: False,
-        pygame.K_w: False,
-        pygame.K_s: False
-    }
-
+    clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    running = False
-                elif event.key in keys_held:
-                    keys_held[event.key] = True
-            if event.type == pygame.KEYUP:
-                if event.key in keys_held:
-                    keys_held[event.key] = False
 
-        if keys_held[pygame.K_a]:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_q]:
+            running = False
+        if keys[pygame.K_a]:
             client_socket.send('a'.encode())
-        if keys_held[pygame.K_d]:
+           # time.sleep(0.1)
+        if keys[pygame.K_d]:
             client_socket.send('d'.encode())
-        if keys_held[pygame.K_w]:
+           # time.sleep(0.1)
+        if keys[pygame.K_w]:
             client_socket.send('w'.encode())
-        if keys_held[pygame.K_s]:
+           # time.sleep(0.1)
+        if keys[pygame.K_s]:
             client_socket.send('s'.encode())
-
+           # time.sleep(0.1)
+        clock.tick(60)
+    
         time.sleep(0.05)  # Small delay to reduce CPU usage
 
     client_socket.close()
